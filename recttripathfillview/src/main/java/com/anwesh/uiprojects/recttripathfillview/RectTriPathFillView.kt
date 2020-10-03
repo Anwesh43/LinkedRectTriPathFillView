ef45a9d356bd0f791370bc63af1162ae00a6b6d6
Anwesh43 <anweshthecool0@gmail.com>
2020-10-04 02:45:26 +0530

    adding RTPFNode class

--- recttripathfillview/src/main/java/com/anwesh/uiprojects/recttripathfillview/RectTriPathFillView.kt
+++ recttripathfillview/src/main/java/com/anwesh/uiprojects/recttripathfillview/RectTriPathFillView.kt
@@ -127,4 +127,45 @@ class RectTriPathFillView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RTPFNode(var i : Int, val state : State = State()) {
+
+        private var next : RTPFNode? = null
+        private var prev : RTPFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RTPFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRTPFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RTPFNode {
+            var curr : RTPFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
