505592aa92fde08e3db8aa4040b7f641d2da4f7c
Anwesh43 <anweshthecool0@gmail.com>
2020-10-04 01:37:52 +0530

    adding constants and libraries

--- recttripathfillview/src/main/java/com/anwesh/uiprojects/recttripathfillview/RectTriPathFillView.kt
+++ recttripathfillview/src/main/java/com/anwesh/uiprojects/recttripathfillview/RectTriPathFillView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.recttripathfillview
 /**
  * Created by anweshmishra on 04/10/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Path
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+        "#3F51B5",
+        "#009688",
+        "#F44336",
+        "#2196F3",
+        "#FFC107"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
