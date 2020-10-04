ca56b30fdb66cb437581cdf1502a186f860be55c
Anwesh43 <anweshthecool0@gmail.com>
2020-10-05 03:34:30 +0530

    adding FPRNode class

--- fillpiperectview/src/main/java/com/anwesh/uiprojects/fillpiperectview/FillPipeRectView.kt
+++ fillpiperectview/src/main/java/com/anwesh/uiprojects/fillpiperectview/FillPipeRectView.kt
@@ -124,4 +124,45 @@ class FillPipeRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FPRNode(var i : Int, val state : State = State()) {
+
+        private var next : FPRNode? = null
+        private var prev : FPRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FPRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFPRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FPRNode {
+            var curr : FPRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
