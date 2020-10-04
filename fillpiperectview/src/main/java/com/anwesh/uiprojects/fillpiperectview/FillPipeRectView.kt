f9ead78ddedea11325ecb2881b6b60d659192ada
Anwesh43 <anweshthecool0@gmail.com>
2020-10-05 03:22:34 +0530

    adding Canvas extension methods

--- fillpiperectview/src/main/java/com/anwesh/uiprojects/fillpiperectview/FillPipeRectView.kt
+++ fillpiperectview/src/main/java/com/anwesh/uiprojects/fillpiperectview/FillPipeRectView.kt
@@ -31,3 +31,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) *n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawFillPipeRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val hSize : Float = h / sizeFactor
+    val wSize : Float = w / sizeFactor
+    val y = h / 2 - hSize
+    val x = w / 2
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawRect(RectF(-wSize / 2, -h / 2 + y * sf2, 0f, -h / 2 + y * sf1), paint)
+        drawRect(RectF(-x * sf2, y, -x * sf2 + x * (1 - sf3), h / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawFPRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFillPipeRect(scale, w, h, paint)
+}
